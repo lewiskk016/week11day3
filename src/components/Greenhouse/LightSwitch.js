224ce75a078761e6ecba10b62ade3bbcb4d4f1b3
Michele Zhang <michelezhang98@gmail.com>
2023-05-17 14:46:57 -0400

    Complete phase 2

--- src/components/Greenhouse/LightSwitch.js
+++ src/components/Greenhouse/LightSwitch.js
@@ -1,11 +1,24 @@
 import './LightSwitch.css';
-
+import { useTheme } from '../../context/ThemeContext';
+import { useState } from 'react';
 
 function LightSwitch() {
+  // const [themeName, setThemeName] = useState("day");
+  const { themeName, setThemeName } = useTheme();
+
+  const handleClick = e => {
+    e.preventDefault();
+    if (themeName === 'day') {
+      setThemeName('night');
+    } else {
+      setThemeName('day');
+    }
+  }
+
   return (
-    <div className="light-switch day">
-      <div className="on">DAY</div>
-      <div className="off">NIGHT</div>
+    <div className={`light-switch ${themeName}`}>
+      <div className="on" onClick={handleClick}>DAY</div>
+      <div className="off" onClick={handleClick}>NIGHT</div>
     </div>
   );
 }
